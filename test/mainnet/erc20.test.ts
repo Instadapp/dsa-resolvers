683a1f3e0940120f7cf1d87e28bb4cf78edb1f08
Daksh Miglani <hello@dak.sh>
2021-06-21 14:12:31 +0530

    test: 💍 move tests to multiple files

--- test/mainnet/erc20.test.ts
+++ test/mainnet/erc20.test.ts
@@ -1,81 +1,16 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
-import { formatEther, formatUnits, parseUnits, parseEther } from "ethers/lib/utils";
+import { parseUnits, parseEther } from "ethers/lib/utils";
 import { ethers, network } from "hardhat";
-import {
-  IERC20__factory,
-  InstaAaveV2Resolver,
-  InstaAaveV2Resolver__factory,
-  InstaCompoundResolver,
-  InstaCompoundResolver__factory,
-  InstaERC20Resolver,
-  InstaERC20Resolver__factory,
-  InstaMakerResolver,
-  InstaMakerResolver__factory,
-} from "../typechain";
-import { Tokens } from "./consts";
+import { IERC20__factory, InstaERC20Resolver, InstaERC20Resolver__factory } from "../../typechain";
+import { Tokens } from "../consts";
 
-describe("Resolvers", () => {
+describe("ERC20 Resolvers", () => {
   let signer: SignerWithAddress;
   const account = "0xa8ABe411d1A3F524a2aB9C54f8427066a1F9f266";
 
   before(async () => {
     [signer] = await ethers.getSigners();
-  });
-
-  describe("Aave V2 Resolver", () => {
-    let resolver: InstaAaveV2Resolver;
-    before(async () => {
-      const deployer = new InstaAaveV2Resolver__factory(signer);
-      resolver = await deployer.deploy();
-      await resolver.deployed();
-    });
-
-    it("Returns the positions on AaveV2", async () => {
-      const results = await resolver.getPosition(account, [Tokens.DAI.addr]);
-      const userTokenData = results[0];
-      const userData = results[1];
-
-      // check for token balances
-      console.log("Supply Balance DAI: ", formatUnits(userTokenData[0].supplyBalance, Tokens.DAI.decimals));
-      expect(userTokenData[0].supplyBalance).to.gt(0);
-      console.log(
-        "Variable Borrow Balance DAI: ",
-        formatUnits(userTokenData[0].variableBorrowBalance, Tokens.DAI.decimals),
-      );
-      expect(userTokenData[0].variableBorrowBalance).to.gt(0);
-
-      // check for user data
-      expect(userData.totalBorrowsETH).to.gt(0);
-      expect(userData.totalCollateralETH).to.gt(0);
-    });
-  });
-
-  describe("Compound V2 Resolver", () => {
-    let resolver: InstaCompoundResolver;
-    before(async () => {
-      const deployer = new InstaCompoundResolver__factory(signer);
-      resolver = await deployer.deploy();
-      await resolver.deployed();
-    });
-
-    it("Returns the price of cToken's underlying in ETH", async () => {
-      const [priceInETH, priceInUSD] = await resolver.getPriceInEth(Tokens.DAI.caddr);
-      console.log("DAI price in ETH: ", formatEther(priceInETH));
-      console.log("DAI price in USD: ", formatEther(priceInUSD));
-      expect(priceInETH).to.gt(0);
-      expect(priceInUSD).to.gt(0);
-    });
-
-    it("Returns the positions correctly", async () => {
-      const [daiData, usdcData] = await resolver.getCompoundData(account, [Tokens.DAI.caddr, Tokens.USDC.caddr]);
-
-      console.log("USDC Balance: ", formatUnits(usdcData.balanceOfUser, 6));
-      expect(usdcData.balanceOfUser).to.gt(0);
-
-      console.log("DAI borrow balance: ", formatEther(daiData.borrowBalanceStoredUser));
-      expect(daiData.borrowBalanceStoredUser).to.gt(0);
-    });
   });
 
   describe("ERC20 Resolver", () => {
@@ -178,21 +113,6 @@ describe("Resolvers", () => {
       for (let i = 0; i < expectedAmts.length; i++) {
         expect(res[i]).eq(expectedAmts[i]);
       }
-    });
-  });
-
-  describe("Maker Resolver", () => {
-    let resolver: InstaMakerResolver;
-
-    before(async () => {
-      const deployer = new InstaMakerResolver__factory(signer);
-      resolver = await deployer.deploy();
-    });
-
-    it("returns the dai position properly", async () => {
-      const res = await resolver.getDaiPosition(account);
-      expect(res.amt).eq(ethers.BigNumber.from("200005872985130003460"));
-      expect(res.dsr).eq(ethers.BigNumber.from("1000000000003170820659990704"));
     });
   });
 });
