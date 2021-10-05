07afb92592d594e66964413ea0e622c6fae7291c
eccheung4 <eccheung4@gmail.com>
2021-10-05 10:22:36 -0700

    feat: initial copy of mainnet files

---
+++ contracts/protocols/polygon/pooltogether/interfaces.sol
@@ -0,0 +1,133 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+interface PrizePoolInterface {
+    function balance() external returns (uint256);
+
+    function maxExitFeeMantissa() external view returns (uint256);
+
+    function reserveTotalSupply() external view returns (uint256);
+
+    function liquidityCap() external view returns (uint256);
+
+    function balanceOfCredit(address user, address controlledToken) external returns (uint256);
+
+    function token() external view returns (address);
+
+    function prizeStrategy() external view returns (address);
+
+    function tokens() external view returns (TokenInterface[] memory);
+
+    function accountedBalance() external view returns (uint256);
+
+    function calculateEarlyExitFee(
+        address from,
+        address controlledToken,
+        uint256 amount
+    ) external returns (uint256 exitFee, uint256 burnedCredit);
+
+    function creditPlanOf(address controlledToken)
+        external
+        view
+        returns (uint128 creditLimitMantissa, uint128 creditRateMantissa);
+
+    function captureAwardBalance() external returns (uint256);
+}
+
+interface TokenFaucetInterface {
+    function asset() external view returns (address);
+
+    function dripRatePerSecond() external view returns (uint256);
+
+    function exchangeRateMantissa() external view returns (uint112);
+
+    function totalUnclaimed() external view returns (uint112);
+
+    function lastDripTimestamp() external view returns (uint32);
+
+    function userStates(address addr) external view returns (uint128, uint128);
+
+    function claim(address user) external returns (uint256);
+}
+
+interface TokenDropInterface {
+    function asset() external view returns (address);
+
+    function measure() external view returns (address);
+
+    function dripRatePerSecond() external view returns (uint256);
+
+    function exchangeRateMantissa() external view returns (uint112);
+
+    function totalUnclaimed() external view returns (uint112);
+
+    function lastDripTimestamp() external view returns (uint32);
+
+    function userStates(address addr) external view returns (uint128, uint128);
+
+    function claim(address user) external returns (uint256);
+}
+
+interface TokenFaucetProxyFactoryInterface {
+    function claimAll(address user, TokenFaucetInterface[] calldata tokenFaucets) external;
+}
+
+interface PodInterface {
+    function prizePool() external view returns (address);
+
+    function getPricePerShare() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function getEarlyExitFee(uint256 amount) external returns (uint256);
+
+    function balanceOfUnderlying(address user) external view returns (uint256 amount);
+
+    function balance() external view returns (uint256);
+
+    function faucet() external view returns (address);
+
+    function tokenDrop() external view returns (address);
+
+    function name() external view returns (string memory);
+
+    function symbol() external view returns (string memory);
+
+    function decimals() external view returns (uint8);
+
+    function totalSupply() external view returns (uint256);
+
+    function token() external view returns (address);
+
+    function ticket() external view returns (address);
+}
+
+interface TokenInterface {
+    function decimals() external view returns (uint8);
+
+    function balanceOf(address) external view returns (uint256);
+
+    function name() external view returns (string memory);
+
+    function symbol() external view returns (string memory);
+}
+
+interface MultiTokenListenerInterface {
+    function getAddresses() external view returns (address[] memory);
+}
+
+interface PrizeStrategyInterface {
+    function prizePeriodRemainingSeconds() external view returns (uint256);
+
+    function isPrizePeriodOver() external view returns (bool);
+
+    function prizePeriodEndAt() external view returns (uint256);
+
+    function calculateNextPrizePeriodStartTime(uint256 currentTime) external view returns (uint256);
+
+    function getExternalErc20Awards() external view returns (address[] memory);
+
+    function getExternalErc721Awards() external view returns (address[] memory);
+
+    function tokenListener() external view returns (address);
+}
---
+++ contracts/protocols/polygon/pooltogether/main.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+import "./interfaces.sol";
+import "./helpers.sol";
+
+contract Resolver is Helpers {
+    function getTokenDropData(address owner, address tokenDropAddress) public view returns (TokenDropData memory) {
+        TokenDropInterface tokenDrop = TokenDropInterface(tokenDropAddress);
+
+        (uint128 lastExchangeRateMantissa, uint128 balance) = tokenDrop.userStates(owner);
+
+        TokenDropData memory tokenDropData = TokenDropData(
+            tokenDrop.asset(),
+            tokenDrop.measure(),
+            tokenDrop.exchangeRateMantissa(),
+            tokenDrop.totalUnclaimed(),
+            tokenDrop.lastDripTimestamp(),
+            lastExchangeRateMantissa,
+            balance
+        );
+
+        return tokenDropData;
+    }
+
+    function getTokenFaucetData(address owner, address tokenFaucetAddress) public returns (TokenFaucetData memory) {
+        TokenFaucetInterface tokenFaucet = TokenFaucetInterface(tokenFaucetAddress);
+
+        (uint128 lastExchangeRateMantissa, uint128 balance) = tokenFaucet.userStates(owner);
+
+        TokenFaucetData memory tokenFaucetData = TokenFaucetData(
+            tokenFaucet.asset(),
+            tokenFaucet.dripRatePerSecond(),
+            tokenFaucet.exchangeRateMantissa(),
+            tokenFaucet.totalUnclaimed(),
+            tokenFaucet.lastDripTimestamp(),
+            lastExchangeRateMantissa,
+            balance,
+            tokenFaucet.claim(owner)
+        );
+
+        return tokenFaucetData;
+    }
+
+    function getPrizeStrategyData(address prizeStrategyAddress) public view returns (PrizeStrategyData memory) {
+        PrizeStrategyInterface prizeStrategy = PrizeStrategyInterface(prizeStrategyAddress);
+
+        PrizeStrategyData memory prizeStrategyData = PrizeStrategyData(
+            address(prizeStrategy),
+            prizeStrategy.prizePeriodRemainingSeconds(),
+            prizeStrategy.isPrizePeriodOver(),
+            prizeStrategy.prizePeriodEndAt(),
+            prizeStrategy.getExternalErc20Awards(),
+            prizeStrategy.getExternalErc721Awards(),
+            prizeStrategy.tokenListener()
+        );
+
+        return prizeStrategyData;
+    }
+
+    function getPosition(address owner, address[] memory prizePoolAddress) public returns (PrizePoolData[] memory) {
+        PrizePoolData[] memory prizePoolsData = new PrizePoolData[](prizePoolAddress.length);
+        for (uint256 i = 0; i < prizePoolAddress.length; i++) {
+            PrizePoolInterface prizePool = PrizePoolInterface(prizePoolAddress[i]);
+            TokenInterface[] memory controlledTokens = prizePool.tokens();
+            ControlledTokenData[] memory controlledTokenData = new ControlledTokenData[](controlledTokens.length);
+
+            for (uint256 j = 0; j < controlledTokens.length; j++) {
+                (uint128 creditLimitMantissa, uint128 creditRateMantissa) = prizePool.creditPlanOf(
+                    address(controlledTokens[j])
+                );
+                controlledTokenData[j] = ControlledTokenData(
+                    address(controlledTokens[j]),
+                    controlledTokens[j].balanceOf(owner),
+                    controlledTokens[j].name(),
+                    controlledTokens[j].symbol(),
+                    controlledTokens[j].decimals(),
+                    creditLimitMantissa,
+                    creditRateMantissa
+                );
+            }
+            PrizeStrategyData memory prizeStrategyData = getPrizeStrategyData(prizePool.prizeStrategy());
+            prizePoolsData[i] = PrizePoolData(
+                prizePool.token(),
+                controlledTokens,
+                prizePool.balance(),
+                prizePool.accountedBalance(),
+                controlledTokenData,
+                prizePool.captureAwardBalance(),
+                prizePool.maxExitFeeMantissa(),
+                prizePool.reserveTotalSupply(),
+                prizePool.liquidityCap(),
+                prizeStrategyData
+            );
+        }
+        return prizePoolsData;
+    }
+
+    function getPodPosition(address owner, address[] memory podAddress) public view returns (PodData[] memory) {
+        PodData[] memory podsData = new PodData[](podAddress.length);
+        for (uint256 i = 0; i < podAddress.length; i++) {
+            PodInterface pod = PodInterface(podAddress[i]);
+
+            podsData[i] = PodData(
+                pod.name(),
+                pod.symbol(),
+                pod.decimals(),
+                pod.prizePool(),
+                pod.getPricePerShare(),
+                pod.balance(),
+                pod.balanceOf(owner),
+                pod.balanceOfUnderlying(owner),
+                pod.totalSupply(),
+                getTokenDropData(owner, pod.tokenDrop()),
+                pod.faucet()
+            );
+        }
+        return podsData;
+    }
+}
+
+contract InstaPoolTogetherResolver is Resolver {
+    string public constant name = "PoolTogether-Resolver-v1";
+}
