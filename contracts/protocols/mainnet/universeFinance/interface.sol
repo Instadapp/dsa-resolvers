a40e1ceb2561c49efc84433d60b50716802c2e66
UniverseFinance <87629076+UniverseFinance@users.noreply.github.com>
2021-11-23 20:00:19 +0800

    Update interface.sol

--- contracts/protocols/mainnet/universeFinance/interface.sol
+++ contracts/protocols/mainnet/universeFinance/interface.sol
@@ -1,21 +1,35 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.7.6;
 pragma abicoder v2;
 
 interface IUniverseAdapter {
+
     function depositProxy(
         address universeVault,
         uint256 amount0,
         uint256 amount1
-    ) external returns (uint256, uint256);
+    ) external returns(uint256, uint256);
+
+}
+
+interface IUniverseResolver {
+
+    function getAllVaultAddress() external view returns(address[] memory vaults);
+
+    function checkUniverseVault(address universeVault) external view returns(bool status);
+
 }
 
 interface IVaultV3 {
-    function getShares(uint256 amount0Desired, uint256 amount1Desired)
-        external
-        view
-        returns (uint256 share0, uint256 share1);
 
-    function getBals(uint256 share0, uint256 share1) external view returns (uint256 amount0, uint256 amount1);
+    function getShares(
+        uint256 amount0Desired,
+        uint256 amount1Desired
+    ) external view returns (uint256 share0, uint256 share1);
+
+    function getBals(
+        uint256 share0,
+        uint256 share1
+    ) external view returns (uint256 amount0, uint256 amount1);
 
     function getUserShares(address user) external view returns (uint256 share0, uint256 share1);
 
@@ -24,23 +38,21 @@ interface IVaultV3 {
     function token1() external view returns (address);
 
     struct MaxShares {
-        uint256 maxShare0;
-        uint256 maxShare1;
+        uint256 maxToken0Amt;
+        uint256 maxToken1Amt;
         uint256 maxSingeDepositAmt0;
         uint256 maxSingeDepositAmt1;
     }
 
     function maxShares() external view returns (MaxShares memory);
 
-    function getTotalAmounts()
-        external
-        view
-        returns (
-            uint256 total0,
-            uint256 total1,
-            uint256 free0,
-            uint256 free1,
-            uint256 utilizationRate0,
-            uint256 utilizationRate1
-        );
+    function getTotalAmounts() external view returns (
+        uint256 total0,
+        uint256 total1,
+        uint256 free0,
+        uint256 free1,
+        uint256 utilizationRate0,
+        uint256 utilizationRate1
+    );
+
 }
