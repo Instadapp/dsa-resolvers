d45feff38d38c1761f57ad429992ac893977fab8
pradyuman-verma <pradyumnverma27@gmail.com>
2022-05-10 20:10:47 +0530

    feat: updated uniswap v3 polygon resolver

--- contracts/protocols/polygon/uniswap/helpers.sol
+++ contracts/protocols/polygon/uniswap/helpers.sol
@@ -231,6 +231,47 @@ abstract contract Helpers is DSMath {
         amount1Min = getMinAmount(TokenInterface(token1), amount1, mintParams.slippage);
     }
 
+    function mintNewAmount(MintParams memory mintParams, int24 newCurrentTick)
+        internal
+        view
+        returns (
+            address token0,
+            address token1,
+            uint256 liquidity,
+            uint256 amount0,
+            uint256 amount1,
+            uint256 amount0Min,
+            uint256 amount1Min
+        )
+    {
+        {
+            (token0, token1) = mintParams.tokenA < mintParams.tokenB
+                ? (mintParams.tokenA, mintParams.tokenB)
+                : (mintParams.tokenB, mintParams.tokenA);
+        }
+
+        // compute the liquidity amount
+        {
+            liquidity = LiquidityAmounts.getLiquidityForAmounts(
+                TickMath.getSqrtRatioAtTick(newCurrentTick),
+                TickMath.getSqrtRatioAtTick(mintParams.lowerTick),
+                TickMath.getSqrtRatioAtTick(mintParams.upperTick),
+                mintParams.amountA,
+                mintParams.amountB
+            );
+
+            (amount0, amount1) = LiquidityAmounts.getAmountsForLiquidity(
+                TickMath.getSqrtRatioAtTick(newCurrentTick),
+                TickMath.getSqrtRatioAtTick(mintParams.lowerTick),
+                TickMath.getSqrtRatioAtTick(mintParams.upperTick),
+                uint128(liquidity)
+            );
+        }
+
+        amount0Min = getMinAmount(TokenInterface(token0), amount0, mintParams.slippage);
+        amount1Min = getMinAmount(TokenInterface(token1), amount1, mintParams.slippage);
+    }
+
     function depositAmount(
         uint256 tokenId,
         uint256 amountA,
@@ -490,5 +531,39 @@ abstract contract Helpers is DSMath {
 
         amount0Min = getMinAmount(TokenInterface(positionInfo.token0), amount0, slippage);
         amount1Min = getMinAmount(TokenInterface(positionInfo.token1), amount1, slippage);
+    }
+
+    function collectInfo(uint256 tokenId) internal returns (uint256 amount0, uint256 amount1) {
+        (
+            ,
+            ,
+            address _token0,
+            address _token1,
+            uint24 _fee,
+            int24 tickLower,
+            int24 tickUpper,
+            uint128 liquidity,
+            uint256 _feeGrowthInside0LastX128,
+            uint256 _feeGrowthInside1LastX128,
+            uint128 tokensOwed0,
+            uint128 tokensOwed1
+        ) = nftManager.positions(tokenId);
+
+        IUniswapV3Pool pool = IUniswapV3Pool(getPoolAddress(_token0, _token1, _fee));
+        // pool.burn(tickLower, tickUpper, 0);
+
+        (, uint256 feeGrowthInside0LastX128, uint256 feeGrowthInside1LastX128, , ) = pool.positions(
+            PositionKey.compute(getUniswapNftManagerAddr(), tickLower, tickUpper)
+        );
+
+        tokensOwed0 += uint128(
+            FullMath.mulDiv(feeGrowthInside0LastX128 - _feeGrowthInside0LastX128, liquidity, FixedPoint128.Q128)
+        );
+        tokensOwed1 += uint128(
+            FullMath.mulDiv(feeGrowthInside1LastX128 - _feeGrowthInside1LastX128, liquidity, FixedPoint128.Q128)
+        );
+
+        amount0 = tokensOwed0;
+        amount1 = tokensOwed1;
     }
 }
