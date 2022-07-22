907b574c2871e2c6da48547f550218284c158c3e
Shriya Tyagi <shriya243@gmail.com>
2022-07-22 16:39:14 +0400

    feat: testcase updated

--- test/mainnet/euler.test.ts
+++ test/mainnet/euler.test.ts
@@ -8,6 +8,16 @@ describe("Euler Resolver", () => {
   let signer: SignerWithAddress;
   const user = "0x9F60699cE23f1Ab86Ec3e095b477Ff79d4f409AD";
   const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  const EUL = "0xd9Fcd98c322942075A5C3860693e9f4f03AAE07b";
+  const agEUR = "0x1a7e4e63778B4f12a199C062f3eFdD288afCBce8";
+  const ENS = "0xC18360217D8F7Ab5e7c516566761Ea12Ce7F9D72";
+  const oSQTH = "0xf1B99e3E573A1a9C5E6B2Ce818b617F0E664E86B";
+  const SHIB = "0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE";
+  const RBN = "0x6123B0049F904d730dB3C36a31167D9d4121fA6B";
+  const CVX = "0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B";
+  const PERP = "0xbC396689893D065F41bc2C6EcbeE5e0085233447";
+  const AXS = "0xBB0E17EF65F82Ab018d8EDd776e8DD940327B28b";
+  const tokens = [WETH, EUL, agEUR, ENS, oSQTH, SHIB, RBN, CVX, PERP, AXS];
 
   before(async () => {
     [signer] = await ethers.getSigners();
@@ -27,7 +37,7 @@ describe("Euler Resolver", () => {
               // eslint-disable-next-line @typescript-eslint/ban-ts-comment
               // @ts-ignore
               jsonRpcUrl: hre.config.networks.hardhat.forking.url,
-              blockNumber: 15131233,
+              blockNumber: 15131933,
             },
           },
         ],
@@ -50,13 +60,16 @@ describe("Euler Resolver", () => {
     });
 
     it("Returns position of active subaccounts", async () => {
-      const response = await resolver.getPositionOfActiveSubAccounts(user, activeTokenIds, [WETH]);
+      const response = await resolver.getPositionOfActiveSubAccounts(user, ["0", "1"], tokens);
       console.log(JSON.stringify(response));
     });
 
     it("Returns the positions of user", async () => {
-      const response = await resolver.getAllPositionsOfUser(user, ["0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"]);
-      console.log(JSON.stringify(response));
+      // const response = await resolver.getAllPositionsOfUser(user, ["0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"]);
+      // console.log(JSON.stringify(response));
+      console.log(
+        (await resolver.getAllPositionsOfUser(user, ["0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"])).toString,
+      );
     });
   });
 });
