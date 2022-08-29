2cdc23c7555453fd7a4d84d03454920a132c2b95
Richa-iitr <richa@cs.iitr.ac.in>
2022-08-29 07:53:44 +0530

    fix: updated main contract

--- contracts/protocols/mainnet/compound_iii/main.sol
+++ contracts/protocols/mainnet/compound_iii/main.sol
@@ -10,30 +10,55 @@ import "./helpers.sol";
 contract CompoundIIIResolver is CompoundIIIHelpers {
     /**
      *@dev get position of the user for all collaterals.
+     *@notice get position details of the user in a market including overall position data, collaterals, rewards etc.
+     *@param user Address of the user whose position details are needed.
+     *@param market Address of the market for which the user's position details are needed.
+     *@return UserData Overall position details of the user including balances, nonce, rewards and flags.
+     *@return UserCollateralData Data related to the collateral assets for which user has the position.
      */
     function getPositionAll(address user, address market)
         public
-        view
         returns (UserData memory, UserCollateralData[] memory)
-    {}
+    {
+        return (getUserData(user, market), getCollateralAll(user, market));
+    }
 
     /**
      *@dev get position of the user for given collateral.
+     *@notice get position details of the user in a market including overall position data, collaterals, rewards etc.
+     *@param user Address of the user whose position details are needed.
+     *@param market Address of the market for which the user's position details are needed
+     *@param tokens IDs or offsets of the token as per comet market whose collateral details are needed.
+     *@return UserData Overall position details of the user including balances, nonce, rewards and flags.
+     *@return UserCollateralData Data related to the assets input for which user's collateral details are needed.
      */
     function getPosition(
         address user,
         address market,
-        address token
-    ) public view returns (UserCollateralData[] memory) {}
-
-    function getMarketConfiguration(address compet) public view returns (MarketConfig memory) {}
+        uint8[] calldata tokens
+    ) public returns (UserData memory, UserCollateralData[] memory) {
+        return (getUserData(user, market), getAssetCollaterals(user, market, tokens));
+    }
 
     /**
-     *@dev get reserves list.
-     *@notice get list of all tokens available in the market.
-     *@return data array of token addresses available in the market.
+     *@dev get market configuration.
+     *@notice returns the market stats including market supplies, balances, rates, flags for market operations,
+     *collaterals or assets active, base asset info etc.
+     *@param comet Address of the comet market for which the user's position details are needed.
+     *@return MarketCofig Struct containing data related to the market and the assets.
      */
-    function getCollateralsList() public view returns (address[] memory data) {}
+    function getMarketConfiguration(address comet) public returns (MarketConfig memory) {
+        return getMarketConfiguration(comet);
+    }
+
+    /**
+     *@dev get collaterals list where user has positiom.
+     *@notice get list of all collaterals in the market.
+     *@return data array of token addresses supported in the market.
+     */
+    function getCollateralsList(address user, address cometMarket) public returns (address[] memory data) {
+        return getList(user, cometMarket);
+    }
 }
 
 contract InstaCompoundIIIResolver is CompoundIIIResolver {
