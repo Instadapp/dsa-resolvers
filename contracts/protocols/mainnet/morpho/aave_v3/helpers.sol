8db6ca95c87271dde4eb7ed46fae6a2265b1f344
q1q0 <carloslin9999@gmail.com>
2023-03-04 10:52:59 -0500

    feat: add aave v3 resolver (POC version)

---
+++ contracts/protocols/mainnet/morpho/aave_v3/helpers.sol
@@ -0,0 +1,320 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+import "./interfaces.sol";
+import { DSMath } from "../../../../utils/dsmath.sol";
+
+contract MorphoHelpers is DSMath {
+    /**
+     *@dev Returns ethereum address
+     */
+    function getEthAddr() internal pure returns (address) {
+        return 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    }
+
+    function getAWethAddr() internal pure returns (address) {
+        return 0x030bA81f1c18d280636F32af80b9AAd02Cf0854e;
+    }
+
+    function getChainlinkEthFeed() internal pure returns (address) {
+        return 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419; // should be updated after deployment
+    }
+
+    function getAaveProtocolDataProvider() internal pure returns (address) {
+        return 0x057835Ad21a177dbdd3090bB1CAE03EaCF78Fc6d; // should be updated after deployment
+    }
+
+    function getAaveIncentivesController() internal pure returns (address) {
+        return 0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5; // should be updated after deployment
+    }
+
+    struct MorphoData {
+        MarketDetail[] aaveMarketsCreated;
+        bool isClaimRewardsPausedAave;
+        uint128 repay;
+        uint128 withdraw;
+        uint256 p2pSupplyAmount;
+        uint256 p2pBorrowAmount;
+        uint256 poolSupplyAmount;
+        uint256 poolBorrowAmount;
+        uint256 totalSupplyAmount;
+        uint256 totalBorrowAmount;
+    }
+
+    struct TokenConfig {
+        // address poolTokenAddress;
+        // address underlyingToken;
+        uint256 decimals;
+        uint256 tokenPriceInEth;
+        uint256 tokenPriceInUsd;
+    }
+
+    struct AaveMarketDetail {
+        uint256 aEmissionPerSecond;
+        uint256 sEmissionPerSecond;
+        uint256 vEmissionPerSecond;
+        uint256 availableLiquidity;
+        uint256 liquidityRate;
+        // uint256 ltv;
+        // uint256 liquidationThreshold;
+        // uint256 liquidationBonus;
+        uint256 totalSupplies;
+        uint256 totalStableBorrows;
+        uint256 totalVariableBorrows;
+    }
+
+    struct MarketDetail {
+        TokenConfig config;
+        // uint256 avgSupplyRatePerYear; //in wad
+        // uint256 avgBorrowRatePerYear; //in wad
+        // uint256 p2pSupplyRate;
+        // uint256 p2pBorrowRate;
+        // uint256 poolSupplyRate;
+        // uint256 poolBorrowRate;
+        // uint256 totalP2PSupply;
+        // uint256 totalPoolSupply;
+        // uint256 totalP2PBorrows;
+        // uint256 totalPoolBorrows;
+        // uint256 p2pSupplyIndex;
+        // uint256 p2pBorrowIndex;
+        // uint256 poolSupplyIndex; //exchange rate of cTokens for compound
+        // uint256 poolBorrowIndex;
+
+        Market market;
+        AaveMarketDetail aaveData;
+    }
+
+    struct Flags {
+        bool isCreated;
+        bool isPaused;
+        bool isPartiallyPaused;
+        bool isP2PDisabled;
+        bool isUnderlyingBorrowEnabled;
+    }
+
+    struct UserMarketData {
+        MarketDetail marketData;
+        // uint256 borrowRatePerYear;
+        // uint256 supplyRatePerYear;
+        uint256 totalSupplies;
+        uint256 totalBorrows;
+        uint256 p2pBorrows;
+        uint256 p2pSupplies;
+        uint256 poolBorrows;
+        uint256 poolSupplies;
+        // uint256 maxWithdrawable;
+        // uint256 maxBorrowable;
+    }
+
+    struct UserData {
+        uint256 borrowable;
+        uint256 debt; //calculated by updating interest accrue indices for all markets
+        uint256 maxDebt; //calculated by updating interest accrue indices for all markets
+        // bool isLiquidatable;
+        // uint256 liquidationThreshold;
+        UserMarketData[] marketData;
+        uint256 ethPriceInUsd;
+        uint256 suppliedValue;
+    }
+
+    struct TokenPrice {
+        uint256 priceInEth;
+        uint256 priceInUsd;
+    }
+
+    IMorphoGetter internal morphoGetter = IMorphoGetter(0x507fA343d0A90786d86C7cd885f5C49263A91FF4);
+    IMorpho internal aaveMorpho = IMorpho(0x777777c9898D384F785Ee44Acfe945efDFf5f3E0);
+    AaveAddressProvider addrProvider = AaveAddressProvider(0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5);
+    IAave internal protocolData = IAave(getAaveProtocolDataProvider());
+    IAave internal incentiveData = IAave(getAaveIncentivesController());
+
+    function getTokensPrices(AaveAddressProvider aaveAddressProvider, address[] memory tokens)
+        internal
+        view
+        returns (TokenPrice[] memory tokenPrices, uint256 ethPrice)
+    {
+        uint256[] memory _tokenPrices = AavePriceOracle(aaveAddressProvider.getPriceOracle()).getAssetsPrices(tokens);
+        ethPrice = uint256(ChainLinkInterface(getChainlinkEthFeed()).latestAnswer());
+        tokenPrices = new TokenPrice[](_tokenPrices.length);
+        for (uint256 i = 0; i < _tokenPrices.length; i++) {
+            tokenPrices[i] = TokenPrice(_tokenPrices[i], wmul(_tokenPrices[i], uint256(ethPrice) * 10**10));
+        }
+    }
+
+    function getLiquidatyData(MarketDetail memory marketData_, address asset)
+        internal
+        view
+        returns (MarketDetail memory)
+    {
+        (, address sToken_, address vToken_) = protocolData.getReserveTokensAddresses(asset);
+
+        (, marketData_.aaveData.aEmissionPerSecond, ) = incentiveData.getAssetData(asset);
+        (, marketData_.aaveData.sEmissionPerSecond, ) = incentiveData.getAssetData(sToken_);
+        (, marketData_.aaveData.vEmissionPerSecond, ) = incentiveData.getAssetData(vToken_);
+        (
+            marketData_.aaveData.availableLiquidity,
+            marketData_.aaveData.totalStableBorrows,
+            marketData_.aaveData.totalVariableBorrows,
+            marketData_.aaveData.liquidityRate,
+            ,
+            ,
+            ,
+            ,
+            ,
+
+        ) = protocolData.getReserveData(asset);
+        return marketData_;
+    }
+
+    function getAaveHelperData(MarketDetail memory marketData_, address poolTokenAddress_)
+        internal
+        view
+        returns (MarketDetail memory)
+    {
+        marketData_.aaveData.totalSupplies = IAToken(poolTokenAddress_).totalSupply();
+        return marketData_;
+    }
+
+    function getAaveMarketData(
+        MarketDetail memory marketData_,
+        address underlying,
+        uint256 priceInEth,
+        uint256 priceInUsd
+    ) internal view returns (MarketDetail memory) {
+        marketData_.market = morphoGetter.market(underlying);
+        // marketData_.config.poolTokenAddress = marketData_.market.aToken;
+        marketData_.config.tokenPriceInEth = priceInEth;
+        marketData_.config.tokenPriceInUsd = priceInUsd;
+
+        marketData_ = getLiquidatyData(marketData_, marketData_.market.aToken);
+        marketData_ = getAaveHelperData(marketData_, marketData_.market.aToken);
+
+        return marketData_;
+    }
+
+    function getMarketData(
+        address underlying,
+        uint256 priceInEth,
+        uint256 priceInUsd
+    ) internal view returns (MarketDetail memory marketData_) {
+        marketData_ = getAaveMarketData(marketData_, underlying, priceInEth, priceInUsd);
+
+        //////////////////////////////////////////////////////
+        // (
+        //     marketData_.avgSupplyRatePerYear,
+        //     marketData_.avgBorrowRatePerYear,
+        //     marketData_.totalP2PSupply,
+        //     marketData_.totalP2PBorrows,
+        //     marketData_.totalPoolSupply,
+        //     marketData_.totalPoolBorrows
+        // ) = morphoGetter.getMainMarketData(underlying);
+
+        // (
+        //     marketData_.p2pSupplyRate,
+        //     marketData_.p2pBorrowRate,
+        //     marketData_.poolSupplyRate,
+        //     marketData_.poolBorrowRate
+        // ) = morphoGetter.getRatesPerYear(underlying);
+
+        // (
+        //     marketData_.p2pSupplyIndex,
+        //     marketData_.p2pBorrowIndex,
+        //     marketData_.poolSupplyIndex,
+        //     marketData_.poolBorrowIndex,
+        //     marketData_.lastUpdateTimestamp,
+        //     marketData_.p2pSupplyDelta,
+        //     marketData_.p2pBorrowDelta
+        // ) = morphoGetter.getAdvancedMarketData(underlying);
+        ///////////////////////////////////////////////////////
+    }
+
+    function getUserMarketData(
+        address user,
+        address underlying,
+        uint256 priceInEth,
+        uint256 priceInUsd
+    ) internal view returns (UserMarketData memory userMarketData_) {
+        userMarketData_.marketData = getMarketData(underlying, priceInEth, priceInUsd);
+
+        userMarketData_.p2pBorrows = morphoGetter.scaledP2PBorrowBalance(underlying, user);
+        userMarketData_.poolBorrows = morphoGetter.scaledPoolBorrowBalance(underlying, user);
+        userMarketData_.totalBorrows = morphoGetter.borrowBalance(underlying, user);
+
+        userMarketData_.p2pSupplies = morphoGetter.scaledP2PSupplyBalance(underlying, user);
+        userMarketData_.poolSupplies = morphoGetter.scaledPoolSupplyBalance(underlying, user);
+        userMarketData_.totalSupplies = morphoGetter.supplyBalance(underlying, user);
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////
+        // userMarketData_.borrowRatePerYear = morphoGetter.getCurrentUserBorrowRatePerYear(underlying, user);
+        // userMarketData_.supplyRatePerYear = morphoGetter.getCurrentUserSupplyRatePerYear(underlying, user);
+
+        // (userMarketData_.maxWithdrawable, userMarketData_.maxBorrowable) = morphoGetter.getUserMaxCapacitiesForAsset(
+        //     user,
+        //     underlying
+        // );
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+    }
+
+    function getUserMarkets(address user) internal view returns (address[] memory userMarkets_) {
+        // address[] memory totalMarkets = morphoGetter.marketsCreated();
+        // for (uint256 i = 0; i < totalMarkets.length; i++) {
+        //     (MarketSideIndexes256 memory supply, MarketSideIndexes256 memory borrow)
+        //     = morphoGetter.updatedIndexes(totalMarkets[i]);
+        // }
+        userMarkets_ = morphoGetter.marketsCreated();
+    }
+
+    function getUserData(address user, address[] memory underlyingList)
+        internal
+        view
+        returns (UserData memory userData_)
+    {
+        uint256 length_ = underlyingList.length;
+
+        UserMarketData[] memory marketData_ = new UserMarketData[](length_);
+        (TokenPrice[] memory tokenPrices, uint256 ethPrice) = getTokensPrices(addrProvider, underlyingList);
+
+        ///////////////////////////////////////////
+        for (uint256 i = 0; i < length_; i++) {
+            marketData_[i] = getUserMarketData(
+                user,
+                underlyingList[i],
+                tokenPrices[i].priceInEth,
+                tokenPrices[i].priceInUsd
+            );
+        }
+        ///////////////////////////////////////////
+
+        userData_.marketData = marketData_;
+        LiquidityData memory data = morphoGetter.liquidityData(user);
+        userData_.borrowable = data.borrowable;
+        userData_.maxDebt = data.maxDebt;
+        userData_.debt = data.debt;
+
+        userData_.ethPriceInUsd = ethPrice;
+    }
+
+    function getMorphoData() internal view returns (MorphoData memory morphoData_) {
+        address[] memory tokens_ = morphoGetter.marketsCreated();
+
+        MarketDetail[] memory aaveMarket_ = new MarketDetail[](tokens_.length);
+        uint256 length_ = tokens_.length;
+
+        (TokenPrice[] memory tokenPrices, ) = getTokensPrices(addrProvider, tokens_);
+
+        for (uint256 i = 0; i < length_; i++) {
+            aaveMarket_[i] = getMarketData(tokens_[i], tokenPrices[i].priceInEth, tokenPrices[i].priceInUsd);
+        }
+
+        morphoData_.aaveMarketsCreated = aaveMarket_;
+
+        morphoData_.isClaimRewardsPausedAave = morphoGetter.isClaimRewardsPaused();
+        Iterations memory itr = morphoGetter.defaultIterations();
+        morphoData_.repay = itr.repay;
+        morphoData_.withdraw = itr.withdraw;
+
+        // (morphoData_.p2pSupplyAmount, morphoData_.poolSupplyAmount, morphoData_.totalSupplyAmount) = morphoGetter
+        //     .getTotalSupply();
+        // (morphoData_.p2pBorrowAmount, morphoData_.poolBorrowAmount, morphoData_.totalBorrowAmount) = morphoGetter
+        //     .getTotalBorrow();
+    }
+}
---
+++ contracts/protocols/mainnet/morpho/aave_v3/interfaces.sol
@@ -0,0 +1,325 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+enum Position {
+    POOL_SUPPLIER,
+    P2P_SUPPLIER,
+    POOL_BORROWER,
+    P2P_BORROWER
+}
+
+/* NESTED STRUCTS */
+
+struct MarketSideDelta {
+    uint256 scaledDelta; // In pool unit.
+    uint256 scaledP2PTotal; // In peer-to-peer unit.
+}
+
+struct Deltas {
+    MarketSideDelta supply;
+    MarketSideDelta borrow;
+}
+
+struct MarketSideIndexes {
+    uint128 poolIndex;
+    uint128 p2pIndex;
+}
+
+struct Indexes {
+    MarketSideIndexes supply;
+    MarketSideIndexes borrow;
+}
+
+struct PauseStatuses {
+    bool isP2PDisabled;
+    bool isSupplyPaused;
+    bool isSupplyCollateralPaused;
+    bool isBorrowPaused;
+    bool isWithdrawPaused;
+    bool isWithdrawCollateralPaused;
+    bool isRepayPaused;
+    bool isLiquidateCollateralPaused;
+    bool isLiquidateBorrowPaused;
+    bool isDeprecated;
+}
+
+/* STORAGE STRUCTS */
+
+// This market struct is able to be passed into memory.
+struct Market {
+    // SLOT 0-1
+    Indexes indexes;
+    // SLOT 2-5
+    Deltas deltas; // 1024 bits
+    // SLOT 6
+    address underlying; // 160 bits
+    PauseStatuses pauseStatuses; // 80 bits
+    // SLOT 7
+    address variableDebtToken; // 160 bits
+    uint32 lastUpdateTimestamp; // 32 bits
+    uint16 reserveFactor; // 16 bits
+    uint16 p2pIndexCursor; // 16 bits
+    // SLOT 8
+    address aToken; // 160 bits
+    // SLOT 9
+    address stableDebtToken; // 160 bits
+    // SLOT 10
+    uint256 idleSupply; // 256 bits
+}
+
+struct Iterations {
+    uint128 repay;
+    uint128 withdraw;
+}
+
+/* STACK AND RETURN STRUCTS */
+
+struct LiquidityData {
+    uint256 borrowable; // The maximum debt value allowed to borrow (in base currency).
+    uint256 maxDebt; // The maximum debt value allowed before being liquidatable (in base currency).
+    uint256 debt; // The debt value (in base currency).
+}
+
+struct IndexesParams {
+    MarketSideIndexes256 lastSupplyIndexes;
+    MarketSideIndexes256 lastBorrowIndexes;
+    uint256 poolSupplyIndex; // The current pool supply index.
+    uint256 poolBorrowIndex; // The current pool borrow index.
+    uint256 reserveFactor; // The reserve factor percentage (10 000 = 100%).
+    uint256 p2pIndexCursor; // The peer-to-peer index cursor (10 000 = 100%).
+    Deltas deltas; // The deltas and peer-to-peer amounts.
+    uint256 proportionIdle; // in ray.
+}
+
+struct GrowthFactors {
+    uint256 poolSupplyGrowthFactor; // The pool's supply index growth factor (in ray).
+    uint256 p2pSupplyGrowthFactor; // Peer-to-peer supply index growth factor (in ray).
+    uint256 poolBorrowGrowthFactor; // The pool's borrow index growth factor (in ray).
+    uint256 p2pBorrowGrowthFactor; // Peer-to-peer borrow index growth factor (in ray).
+}
+
+struct MarketSideIndexes256 {
+    uint256 poolIndex;
+    uint256 p2pIndex;
+}
+
+struct Indexes256 {
+    MarketSideIndexes256 supply;
+    MarketSideIndexes256 borrow;
+}
+
+struct Signature {
+    uint8 v;
+    bytes32 r;
+    bytes32 s;
+}
+
+struct MatchingEngineVars {
+    address underlying;
+    MarketSideIndexes256 indexes;
+    uint256 amount;
+    uint256 maxIterations;
+    bool borrow;
+    function(address, address, uint256, uint256, bool) updateDS; // This function will be used to update the data-structure.
+    bool demoting; // True for demote, False for promote.
+    function(uint256, uint256, MarketSideIndexes256 memory, uint256) pure returns (uint256, uint256, uint256) step; // This function will be used to decide whether to use the algorithm for promoting or for demoting.
+}
+
+// struct LiquidityVars {
+//     address user;
+//     IAaveOracle oracle;
+//     DataTypes.EModeCategory eModeCategory;
+// }
+
+struct PromoteVars {
+    address underlying;
+    uint256 amount;
+    uint256 p2pIndex;
+    uint256 maxIterations;
+    function(address, uint256, uint256) returns (uint256, uint256) promote;
+}
+
+struct BorrowWithdrawVars {
+    uint256 onPool;
+    uint256 inP2P;
+    uint256 toWithdraw;
+    uint256 toBorrow;
+}
+
+struct SupplyRepayVars {
+    uint256 onPool;
+    uint256 inP2P;
+    uint256 toSupply;
+    uint256 toRepay;
+}
+
+struct LiquidateVars {
+    uint256 closeFactor;
+    uint256 seized;
+}
+
+struct AmountToSeizeVars {
+    uint256 liquidationBonus;
+    uint256 borrowedTokenUnit;
+    uint256 collateralTokenUnit;
+    uint256 borrowedPrice;
+    uint256 collateralPrice;
+}
+
+// Max gas to consume during the matching process for supply, borrow, withdraw and repay functions.
+struct MaxGasForMatching {
+    uint64 supply;
+    uint64 borrow;
+    uint64 withdraw;
+    uint64 repay;
+}
+
+struct AssetLiquidityData {
+    uint256 collateralValue; // The collateral value of the asset.
+    uint256 maxDebtValue; // The maximum possible debt value of the asset.
+    uint256 debtValue; // The debt value of the asset.
+    uint256 underlyingPrice; // The price of the token.
+    uint256 collateralFactor; // The liquidation threshold applied on this token.
+}
+
+interface IMorphoGetter {
+    function POOL() external view returns (address);
+
+    function ADDRESSES_PROVIDER() external view returns (address);
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+
+    function E_MODE_CATEGORY_ID() external view returns (uint256);
+
+    function market(address underlying) external view returns (Market memory);
+
+    function marketsCreated() external view returns (address[] memory);
+
+    function scaledCollateralBalance(address underlying, address user) external view returns (uint256);
+
+    function scaledP2PBorrowBalance(address underlying, address user) external view returns (uint256);
+
+    function scaledP2PSupplyBalance(address underlying, address user) external view returns (uint256);
+
+    function scaledPoolBorrowBalance(address underlying, address user) external view returns (uint256);
+
+    function scaledPoolSupplyBalance(address underlying, address user) external view returns (uint256);
+
+    function supplyBalance(address underlying, address user) external view returns (uint256);
+
+    function borrowBalance(address underlying, address user) external view returns (uint256);
+
+    function collateralBalance(address underlying, address user) external view returns (uint256);
+
+    function userCollaterals(address user) external view returns (address[] memory);
+
+    function userBorrows(address user) external view returns (address[] memory);
+
+    function isManaging(address delegator, address manager) external view returns (bool);
+
+    function userNonce(address user) external view returns (uint256);
+
+    function defaultIterations() external view returns (Iterations memory);
+
+    function positionsManager() external view returns (address);
+
+    function rewardsManager() external view returns (address);
+
+    function treasuryVault() external view returns (address);
+
+    function isClaimRewardsPaused() external view returns (bool);
+
+    function updatedIndexes(address underlying) external view returns (Indexes256 memory);
+
+    function liquidityData(address user) external view returns (LiquidityData memory);
+
+    function getNext(
+        address underlying,
+        Position position,
+        address user
+    ) external view returns (address);
+
+    function getBucketsMask(address underlying, Position position) external view returns (uint256);
+}
+
+interface IMorpho is IMorphoGetter {}
+
+interface IAave {
+    function getReserveData(address asset)
+        external
+        view
+        returns (
+            uint256 availableLiquidity,
+            uint256 totalStableDebt,
+            uint256 totalVariableDebt,
+            uint256 liquidityRate,
+            uint256 variableBorrowRate,
+            uint256 stableBorrowRate,
+            uint256 averageStableBorrowRate,
+            uint256 liquidityIndex,
+            uint256 variableBorrowIndex,
+            uint40 lastUpdateTimestamp
+        );
+
+    function getAssetData(address asset)
+        external
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256
+        );
+
+    function getReserveTokensAddresses(address asset)
+        external
+        view
+        returns (
+            address aTokenAddress,
+            address stableDebtTokenAddress,
+            address variableDebtTokenAddress
+        );
+
+    function getReserveConfigurationData(address asset)
+        external
+        view
+        returns (
+            uint256 decimals,
+            uint256 ltv,
+            uint256 liquidationThreshold,
+            uint256 liquidationBonus,
+            uint256 reserveFactor,
+            bool usageAsCollateralEnabled,
+            bool borrowingEnabled,
+            bool stableBorrowRateEnabled,
+            bool isActive,
+            bool isFrozen
+        );
+}
+
+interface AaveAddressProvider {
+    function getLendingPool() external view returns (address);
+
+    function getPriceOracle() external view returns (address);
+}
+
+interface AavePriceOracle {
+    function getAssetPrice(address _asset) external view returns (uint256);
+
+    function getAssetsPrices(address[] calldata _assets) external view returns (uint256[] memory);
+
+    function getSourceOfAsset(address _asset) external view returns (uint256);
+
+    function getFallbackOracle() external view returns (uint256);
+}
+
+interface ChainLinkInterface {
+    function latestAnswer() external view returns (int256);
+
+    function decimals() external view returns (uint256);
+}
+
+interface IAToken {
+    function UNDERLYING_ASSET_ADDRESS() external view returns (address);
+
+    function totalSupply() external view returns (uint256);
+}
